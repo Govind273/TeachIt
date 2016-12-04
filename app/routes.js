e8919ecdee46f973e5295f8cfbb1b45919682f52
jashjhav <jashjhav@iu.edu>
2016-12-04 17:03:02 -0500

    Added OAuth for Facebook and Gmail

--- app/routes.js
+++ app/routes.js
@@ -1208,6 +1208,60 @@ module.exports = function(app, passport,server, mongoose, Grid, fs) {
 		})
 	});
 
+	// GET /auth/facebook
+// Use passport.authenticate() as route middleware to authenticate the
+// request. The first step in Facebook authentication will involve
+// redirecting the user to facebook.com. After authorization, Facebook will
+// redirect the user back to this application at /auth/facebook/callback
+		app.get('/auth/facebook',
+  			passport.authenticate('facebook',{ scope : 'email' }));
+
+// GET /auth/facebook/callback
+// Use passport.authenticate() as route middleware to authenticate the
+// request. If authentication fails, the user will be redirected back to the
+// login page. Otherwise, the primary route function function will be called,
+// which, in this example, will redirect the user to the home page.
+		app.get('/auth/facebook/callback',
+  			passport.authenticate('facebook', { 
+				successRedirect : '/login', 	
+				failureRedirect: '/login' }));
+
+// GET /auth/google
+// Use passport.authenticate() as route middleware to authenticate the
+// request. The first step in Google authentication will involve
+// redirecting the user to google.com. After authorization, Google
+// will redirect the user back to this application at /auth/google/callback
+app.get('/auth/google',
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+// GET /auth/google/callback
+// Use passport.authenticate() as route middleware to authenticate the
+// request. If authentication fails, the user will be redirected back to the
+// login page. Otherwise, the primary route function function will be called,
+// which, in this example, will redirect the user to the home page.
+app.get('/auth/google/callback',
+  passport.authenticate('google', { 
+				successRedirect : '/login', 	
+				failureRedirect: '/login' }));
+
+// GET /auth/facebook
+// Use passport.authenticate() as route middleware to authenticate the
+// request. The first step in Facebook authentication will involve
+// redirecting the user to facebook.com. After authorization, Facebook will
+// redirect the user back to this application at /auth/facebook/callback
+		app.get('/auth/github',
+  			passport.authenticate('github',{ scope : 'user:email' }));
+
+// GET /auth/facebook/callback
+// Use passport.authenticate() as route middleware to authenticate the
+// request. If authentication fails, the user will be redirected back to the
+// login page. Otherwise, the primary route function function will be called,
+// which, in this example, will redirect the user to the home page.
+		app.get('/auth/github/callback',
+  			passport.authenticate('github', { 
+				successRedirect : '/login', 	
+				failureRedirect: '/login' }));
+
 	/* Always place this at the bottom to handle all paths that do not exist.*/
 	app.all('*', function(req,res) {
 		res.redirect('/login');
